9b8ace772c6fa329ecbf2115cfd78b33479f1048
york <york0704@gmail.com>
2022-07-29 22:52:12 +0800

    Update 2022-07-29T14:52:04.292Z

--- app/assets/js/all.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  $('.hamburger').click(function (e) { 
-    e.preventDefault();
-    $('.dropMenu').toggleClass('active');
-  });
-});
---
+++ assets/js/all.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.hamburger').click(function (e) {
+    e.preventDefault();
+    $('.dropMenu').toggleClass('active');
+  });
+});
+//# sourceMappingURL=all.js.map
