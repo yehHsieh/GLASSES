660feddaa2a534876289a6a985b58502a9903c02
york <york0704@gmail.com>
2022-08-09 22:16:11 +0800

    add map&location

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,4 +3,10 @@ $(document).ready(function(){
     e.preventDefault();
     $('.dropMenu').toggleClass('active');
   });
+
+  // $('.topNav li').click(function (e) { 
+  //   e.preventDefault();
+  //   $(this).toggleClass('navBottom');
+  //   $(this).parent().siblings().find('li').removeClass('navBottom');
+  // });
 });
