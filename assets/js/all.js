bb3cde986d9f8916af28adab0ebc6acadbebdf0f
york <york0704@gmail.com>
2022-08-13 00:13:22 +0800

    Update 2022-08-12T16:13:15.658Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,6 +4,9 @@ $(document).ready(function () {
   $('.hamburger').click(function (e) {
     e.preventDefault();
     $('.dropMenu').toggleClass('active');
-  });
+  }); // $('.topNav li').click(function (e) { 
+  //   e.preventDefault();
+  //   $(this).addClass('navBottom').siblings().removeClass('navBottom');
+  // });
 });
 //# sourceMappingURL=all.js.map
